19f67ff700479da368d90a58eaad802870113731
larhra <agbajeaminah@yahoo.com>
2025-02-23 21:54:06 +0100

    new commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
-export default function Footert (){
-    return (
-        <footer>
-             <p>© 2023 City Lovers</p>
-        </footer>
+function Footer() {
+    return(
+        <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+           <p>© 2023 City Lovers</p>
+      </footer> 
     );
 }
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-export default function Header () {
+function Header() {
     return (
-    <header>
-        <h1>My Favorite Cities</h1>
-    </header>
-    );
+        <div>
+           <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+              <h1>My Favorite Cities</h1>
+           </header>
+        </div>
+        );
 }
+
+export default Header;
